fffc88f2451f496fac2627d5ef85e2b135fbeff1
Emiliano Garcia <60363956+EmilianoGarciaLopez@users.noreply.github.com>
2024-12-28 23:38:25 -0500

    github workflow

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: '/', // Use root path since we're using a custom domain
+    base: '/AI-timeline/', // Use root path since we're using a custom domain
 });
