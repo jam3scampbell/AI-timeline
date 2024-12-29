9fd9809921dc8b7df2ca398e3a557db25ee3912e
James Campbell <70195999+jam3scampbell@users.noreply.github.com>
2024-12-29 02:36:47 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: '/AI-timeline/', // Use root path since we're using a custom domain
+    base: '/', // Use root path since we're using a custom domain
 });
