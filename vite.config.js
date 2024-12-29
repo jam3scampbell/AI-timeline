2b39901abc94f1cb2d6d739d2d91495a9cc38ddf
Emiliano Garcia <60363956+EmilianoGarciaLopez@users.noreply.github.com>
2024-12-28 21:58:49 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    // If deploying to a sub-path, set base: "/subpath/", etc.
+    base: '/', // Use root path since we're using a custom domain
 });
