68f19fa6c978bc1dc317886bb0ce823c59c46761
Emiliano Garcia <60363956+EmilianoGarciaLopez@users.noreply.github.com>
2024-12-31 22:46:05 -0500

    removed sticky

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -377,7 +377,7 @@ export default function Timeline() {
 
     return (
         <>
-            <div className="my-2 sticky top-0 z-20 py-2 max-w-[1400px] mx-auto px-4">
+            <div className="my-2 top-0 z-20 py-2 max-w-[1400px] mx-auto px-4">
                 {/* Use flex-col on mobile, row on larger screens */}
                 <div className="flex flex-col sm:flex-row gap-4 sm:gap-2">
                     {/* Categories section */}
@@ -421,7 +421,7 @@ export default function Timeline() {
             </div>
             <div
                 ref={containerRef}
-                className="relative mx-auto max-w-[1400px] overflow-x-scroll overflow-y-hidden timeline-container"
+                className="relative mx-auto max-w-[1400px] overflow-x-scroll timeline-container"
                 onWheel={handleWheel}
             >
                 <div
