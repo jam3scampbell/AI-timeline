1a258838418e61223454dece7dd822d3e7925d9e
Emiliano Garcia <60363956+EmilianoGarciaLopez@users.noreply.github.com>
2025-01-25 14:55:26 -0500

    made google analytics fully compatible with vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePluginRadar({
+            analytics: {
+                id: 'G-NGY6H64ENF',
+            },
+        })
+    ],
     base: '/', // Use root path since we're using a custom domain
 });
