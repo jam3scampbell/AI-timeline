cef9fa6be46986022f2f219400c5932ff0e2b2a7
Emiliano Garcia <60363956+EmilianoGarciaLopez@users.noreply.github.com>
2025-02-04 20:42:54 -0500

    bumped vite

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -10,12 +10,12 @@ export default function LanguageSwitcher() {
   };
 
   return (
-    <div className="language-switcher">
+    <div className="bg-white/10 text-white px-4 mx-2 py-1 rounded hover:bg-white/20 transition whitespace-nowrap backdrop-blur-[1px] cursor-pointer">
       <select
         value={i18n.language}
         onChange={handleLanguageChange}
         // Styling similar to timeline control buttons
-        className="bg-white/10 text-white px-4 py-1 rounded hover:bg-white/20 transition whitespace-nowrap backdrop-blur-[1px] focus:outline-none cursor-pointer"
+        className=" text-white bg-transparent focus:outline-none rounded  whitespace-nowrap "
       >
         <option value="en">English</option>
         <option value="zh">中文</option>
